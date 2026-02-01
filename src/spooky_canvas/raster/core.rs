f0ad378d83bdcba94e4ef98e623254be6af93e82
human-31 <mikesu31@icloud.com>
2026-02-01 10:58:23 -0600

    added some tests for fill_all variants

--- src/spooky_canvas/raster/core.rs
+++ src/spooky_canvas/raster/core.rs
@@ -1,3 +1,5 @@
+use rayon::iter;
+
 use super::super::canvas::Canvas;
 use super::tests;
 
@@ -61,14 +63,91 @@ pub fn triangle(
 
 #[allow(dead_code)]
 pub fn main_test() {
-    /*
-    kinds of tests to run for test_fill_all():
-      - small square
-      - large square
-      - small wide rectangle
-      - large wide rectangle
-      - small tall rectangle
-      - large tall rectangle
-     */
-    tests::fill_all_table(1031, 1031, 10, 100);
+    println!("===============================================================");
+    println!();
+
+    if false {
+        let sets = 5;
+        let small_iter = 5;
+        let medium_iter = 50;
+        let large_iter = 500;
+        let xlarge_iter = 5000;
+
+        tests::fill_all_table(1, 1, sets, small_iter, true);
+        tests::fill_all_table(1, 1, sets, medium_iter, true);
+        tests::fill_all_table(1, 1, sets, large_iter, true);
+        tests::fill_all_table(1, 1, sets, xlarge_iter, true);
+
+        println!("=============================================================");
+        println!();
+        
+        tests::fill_all_table(10, 10, sets, small_iter, true);
+        tests::fill_all_table(10, 10, sets, medium_iter, true);
+        tests::fill_all_table(10, 10, sets, large_iter, true);
+        tests::fill_all_table(10, 10, sets, xlarge_iter, true);
+        
+        println!("=============================================================");
+        println!();
+        
+        tests::fill_all_table(100, 100, sets, small_iter, true);
+        tests::fill_all_table(100, 100, sets, medium_iter, true);
+        tests::fill_all_table(100, 100, sets, large_iter, true);
+        tests::fill_all_table(100, 100, sets, xlarge_iter, true);
+    }
+
+    if false {
+        /*
+        kinds of tests to run for test_fill_all():
+          - small square
+          - large square
+          - small wide rectangle
+          - large wide rectangle
+          - small tall rectangle
+          - large tall rectangle
+        */
+
+        let sets = 10;
+        let small_iter = 1;
+        let large_iter = 10;
+
+        // 1:1 small
+        tests::fill_all_table(144, 144, sets, small_iter, true);
+        tests::fill_all_table(144, 144, sets, large_iter, true);
+        
+        // 1:1 large
+        tests::fill_all_table(1440, 1440, sets, small_iter, true);
+        tests::fill_all_table(1440, 1440, sets, large_iter, true);
+        
+        // 16:9 small
+        tests::fill_all_table(144, 81, sets, small_iter, true);
+        tests::fill_all_table(144, 81, sets, large_iter, true);
+        
+        // 16:9 large
+        tests::fill_all_table(1440, 810, sets, small_iter, true);
+        tests::fill_all_table(1440, 810, sets, large_iter, true);
+        
+        // 9:16 small
+        tests::fill_all_table(81, 144, sets, small_iter, true);
+        tests::fill_all_table(81, 144, sets, large_iter, true);
+        
+        // 9:16 large
+        tests::fill_all_table(810, 1440, sets, small_iter, true);
+        tests::fill_all_table(810, 1440, sets, large_iter, true);
+
+        // 4:3 small
+        tests::fill_all_table(144, 108, sets, small_iter, true);
+        tests::fill_all_table(144, 108, sets, large_iter, true);
+        
+        // 4:3 large
+        tests::fill_all_table(1440, 1080, sets, small_iter, true);
+        tests::fill_all_table(1440, 1080, sets, large_iter, true);
+        
+        // 3:4 small
+        tests::fill_all_table(108, 144, sets, small_iter, true);
+        tests::fill_all_table(108, 144, sets, large_iter, true);
+        
+        // 3:4 large
+        tests::fill_all_table(1080, 1440, sets, small_iter, true);
+        tests::fill_all_table(1080, 1440, sets, large_iter, true);
+    }
 }
