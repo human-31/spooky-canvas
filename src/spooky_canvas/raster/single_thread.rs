4949e934a6ca338bb8742b39ab73fcd36f6950c4
human-31 <mikesu31@icloud.com>
2026-02-10 11:18:46 -0600

    Added rect function

--- src/spooky_canvas/raster/single_thread.rs
+++ src/spooky_canvas/raster/single_thread.rs
@@ -18,11 +18,48 @@ use super::super::canvas::Canvas;
 /// using a single-thread.
 pub fn fill_all(
     canvas: &mut Canvas,
-    r: u8, g: u8, b: u8, a: u8,
+    rgba: [u8; 4]
 ) {
-    let rgba = [r, g, b, a];
-
     for pixel in canvas.pixels.chunks_exact_mut(4) {
         pixel.copy_from_slice(&rgba);
     }
+}
+
+pub fn rect_xy(
+    canvas: &mut Canvas,
+    x: i32, y: i32,
+    width: u32, height: u32,
+    rgba: [u8; 4],
+) {
+    let x0 = x.max(0) as u32;
+    let y0 = y.max(0) as u32;
+
+    let x1 = ((x + width as i32) as u32).min(canvas.width() - 1);
+    let y1 = ((y + height as i32) as u32).min(canvas.height() - 1);
+
+    for py in y0..y1 {
+        for px in x0..x1 {
+            let i = canvas.xy_to_index_unchecked(px, py);
+            canvas.pixels[i..(i + 4)].copy_from_slice(&rgba);
+        }
+    }
+}
+
+pub fn rect_by_row(
+    canvas: &mut Canvas,
+    x: i32, y: i32,
+    width: u32, height: u32,
+    rgba: [u8; 4],
+) {
+
+}
+
+pub fn triangle_xy(
+    canvas: &mut Canvas,
+    x1: i32, y1: i32,
+    x2: i32, y2: i32,
+    x3: i32, y3: i32,
+    rgba: [u8; 4],
+) {
+
 }
